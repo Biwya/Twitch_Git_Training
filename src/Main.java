910c064cd489a9c59e0f1e2925302656b03a5b28
Biwya <radu.net@gmail.com>
2023-06-24 20:15:39 +0200

    Add end message

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello and welcome the raid party and everyone else from Twitch to this course!");
+
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i + ";");
         }
+        System.out.println("Hope you enjoy it and learn something new!");
     }
 }
