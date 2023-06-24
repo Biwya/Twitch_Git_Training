b352aedc0334988802d3b1a84e2ddc1fae3ce423
Biwya <radu.net@gmail.com>
2023-06-24 20:02:48 +0200

    Welcome the raid party!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome to this course!");
+        System.out.println("Hello and welcome to raid party!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i + ";");
         }
